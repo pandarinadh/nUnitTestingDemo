2e550cad9f5bf3df79ee70f6a1f9b220f1beb1b6
pandarinadh bikkumalla <pandari.bikkumalla@dycominc.com>
2019-06-04 16:22:43 -0400

    modal dialog update

--- ASPNETMVCFilter/Scripts/Registration/AngularControllers/InstructorsController.js
+++ ASPNETMVCFilter/Scripts/Registration/AngularControllers/InstructorsController.js
@@ -1,7 +1,24 @@
 ﻿'use strict';
 
-registrationModule.controller("InstructorsController", function ($scope, myData) {
+registrationModule.controller("InstructorsController",['$scope', '$mdDialog','myData', function ($scope, $mdDialog, myData) {
     console.log(myData);
     $scope.instructors = myData.instructors;
-}
+
+    $scope.showDialog = function ($event) {
+        alert = $mdDialog.alert({
+            title: 'Attention',
+            textContent: 'This is an example of how easy dialogs can be!',
+            ok: 'Close'
+        });
+
+        setTimeout(function () { console.log('in timer'); $mdDialog.hide(); }, 1000);
+
+        $mdDialog
+          .show(alert)
+          .finally(function () {
+              setTimeout
+              alert = undefined;
+          });
+    };
+}]
 );
--- ASPNETMVCFilter/Scripts/Registration/RegistrationModule.js
+++ ASPNETMVCFilter/Scripts/Registration/RegistrationModule.js
@@ -1,7 +1,8 @@
 ﻿'use strict';
 
-var registrationModule = angular.module("RegistrationModule",['ngRoute'])
+var registrationModule = angular.module("RegistrationModule", ['ngRoute', 'ngMaterial'])
     .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/Registration', { templateUrl: '/templates/courses.html', controller: 'CoursesController' });
         $routeProvider.when('/Registration/Courses', { templateUrl: '/templates/courses.html', controller: 'CoursesController' });
         $routeProvider.when('/Registration/Instructors', { templateUrl: '/templates/instructors.html', controller: 'InstructorsController' });
         $locationProvider.html5Mode({
